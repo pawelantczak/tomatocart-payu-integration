5f3945c06b7f11ccd67a0cf45ea8a7712497a3b9
Paweł Antczak <pawel@antczak.org>
2014-03-19 00:10:31 +0100

    Admin config, Success and Error URLs

--- admin/includes/modules/payment/payu.php
+++ admin/includes/modules/payment/payu.php
@@ -27,7 +27,7 @@ class osC_Payment_payu extends osC_Payment_Admin {
 	 * @var string
 	 * @access private
 	 */
-	var $_author_name = 'PaweÅ‚ Antczak';
+	var $_author_name = 'Pawel‚ Antczak';
 	
 	/**
 	 * The developers address
@@ -79,8 +79,12 @@ class osC_Payment_payu extends osC_Payment_Admin {
 		
 		parent::install ();
 		
-	$osC_Database->simpleQuery ( "insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, use_function, set_function, date_added) values ('Enable PayU Module', 'MODULE_PAYMENT_PAYU_STATUS', '-1', 'Do you want to accept PayU payments?', '6', '0', 'osc_cfg_use_get_boolean_value', 'osc_cfg_set_boolean_value(array(1, -1))', now())" );
-	
+		$osC_Database->simpleQuery ( "insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, use_function, set_function, date_added) values ('Enable PayU Module', 'MODULE_PAYMENT_PAYU_STATUS', '-1', 'Do you want to accept PayU payments?', '6', '1', 'osc_cfg_use_get_boolean_value', 'osc_cfg_set_boolean_value(array(1, -1))', now())" );
+		$osC_Database->simpleQuery ( "insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('POS ID', 'MODULE_PAYMENT_PAYU_POS_ID', '', 'POS ID given by PayU.', '6', '2', now())" );
+		$osC_Database->simpleQuery ( "insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('KEY #1', 'MODULE_PAYMENT_PAYU_KEY_1', '', 'First key given by PayU.', '6', '3', now())" );
+		$osC_Database->simpleQuery ( "insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('KEY #2', 'MODULE_PAYMENT_PAYU_KEY_2', '', 'Second key given by PayU.', '6', '4', now())" );
+		$osC_Database->simpleQuery ( "insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('POS Authentication Key', 'MODULE_PAYMENT_PAYU_POS_AUTH_KEY', '', 'POS Authentication Key given by PayU.', '6', '5', now())" );
+		$osC_Database->simpleQuery ( "insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Sort order of display', 'MODULE_PAYMENT_PAYU_SORT_ORDER', '0', 'Sort order of display. Lowest is displayed first.', '6', '6', now())" );
 	}
 	
 	/**
@@ -93,9 +97,11 @@ class osC_Payment_payu extends osC_Payment_Admin {
 		if (! isset ( $this->_keys )) {
 			$this->_keys = array (
 					'MODULE_PAYMENT_PAYU_STATUS',
-					'MODULE_PAYMENT_PAYU_SORT_ORDER',
-					'MODULE_PAYMENT_PAYU_ORDER_STATUS_ID',
-					'MODULE_PAYMENT_PAYU_SERVER' 
+					'MODULE_PAYMENT_PAYU_POS_ID',
+					'MODULE_PAYMENT_PAYU_KEY_1',
+					'MODULE_PAYMENT_PAYU_KEY_2',
+					'MODULE_PAYMENT_PAYU_POS_AUTH_KEY',
+					'MODULE_PAYMENT_PAYU_SORT_ORDER' 
 			);
 		}
 		
--- includes/modules/payment/payu.php
+++ includes/modules/payment/payu.php
@@ -10,8 +10,7 @@ class osC_Payment_payu extends osC_Payment {
 		$this->_status = (MODULE_PAYMENT_PAYU_STATUS == '1') ? true : false;
 		$this->_sort_order = MODULE_PAYMENT_PAYU_SORT_ORDER;
 		
-		$this->form_action_url = 'https://www.platnosci.pl/paygw/UTF/NewPayment';
-		$this->form_action_url = 'http://localhost/save.php';
+		$this->form_action_url = 'https://secure.payu.com/paygw/UTF/NewPayment';
 	}
 	
 	// Select payment method
@@ -29,13 +28,106 @@ class osC_Payment_payu extends osC_Payment {
 	
 	// Set variables for provider
 	function process_button() {
-		$fields = osc_draw_hidden_field ( 'pawel', antczak );
+		global $osC_ShoppingCart, $osC_Customer;
 		
-		return $fields;
+		$params = array (
+				'pos_id' => MODULE_PAYMENT_PAYU_POS_ID,
+				'pos_auth_key' => MODULE_PAYMENT_PAYU_POS_AUTH_KEY,
+				'session_id' => md5 ( $this->_order_id . $osC_ShoppingCart->getTotal () ),
+				'amount' => $osC_ShoppingCart->getTotal (),
+				'desc' => STORE_NAME,
+				'order_id' => $this->_order_id,
+				'first_name' => $osC_ShoppingCart->getBillingAddress ( 'firstname' ),
+				'last_name' => $osC_ShoppingCart->getBillingAddress ( 'lastname' ),
+				'email' => $osC_Customer->getEmailAddress (),
+				'client_ip' => $_SERVER ['REMOTE_ADDR'] 
+		);
+		
+		$process_button_string = '';
+		foreach ( $params as $key => $value ) {
+			$key = trim ( $key );
+			$value = trim ( $value );
+			$process_button_string .= osc_draw_hidden_field ( $key, $value );
+			$process_button_string .= "\n";
+		}
+		
+		return $process_button_string;
 	}
 	
 	// Receive callback
+	// app.antczak.org/tomato/checkout.php?callback&module=payu&status=error&errorCode=%error%&orderId=%orderId%
 	function callback() {
+		global $osC_ShoppingCart, $osC_Currencies, $osC_Language, $osC_Tax, $messageStack, $osC_Database;
+		
+		switch ($_GET ['status']) {
+			case "error" :
+				osC_Order::process ( $_GET ['orderId'], ORDERS_STATUS_CANCELLED, $this->_getErrorMessage ( $_GET ['errorCode'] ) );
+				osC_Order::insertOrderStatusHistory ( $_GET ['orderId'], ORDERS_STATUS_CANCELLED, $this->_getErrorMessage ( $_GET ['errorCode'] ) );
+				$messageStack->add_session(FILENAME_CHECKOUT, $this->_getErrorMessage ( $_GET ['errorCode'] ), 'error');
+				osc_redirect ( osc_href_link ( FILENAME_CHECKOUT, 'error', 'SSL', true, true, true ) );
+				break;
+			case "ok" :
+				osC_Order::process ( $_GET ['orderId'], ORDERS_STATUS_PAID, 'Ok' );
+				osC_Order::insertOrderStatusHistory ( $_GET ['orderId'], ORDERS_STATUS_PAID, 'Ok' );
+				$osC_ShoppingCart->reset ( true );
+				osc_redirect ( osc_href_link ( FILENAME_CHECKOUT, 'success', 'SSL', true, true, true ) );
+				
+				break;
+			case "update" :
+				echo "OK";
+				break;
+		}
+	}
+	function _getErrorMessage($errorCode) {
+		if (! isset ( $this->_error_messages )) {
+			$this->_loadErrorMessages ();
+		}
+		
+		return $this->_error_messages [$errorCode];
+	}
+	function _loadErrorMessages() {
+		$this->_error_messages = array (
+				'100' => 'brak lub błędna wartość parametru pos_id',
+				'101' => 'brak parametru session_id',
+				'102' => 'brak parametru ts',
+				'103' => 'brak lub błędna wartość parametru sig',
+				'104' => 'brak parametru desc',
+				'105' => 'brak parametru client_ip',
+				'106' => 'brak parametru first_name',
+				'107' => 'brak parametru last_name',
+				'108' => 'brak parametru street',
+				'109' => 'brak parametru city',
+				'110' => 'brak parametru post_code',
+				'111' => 'brak parametru amount (lub/oraz amount_netto dla usługi SMS)',
+				'112' => 'błędny numer konta bankowego',
+				'113' => 'brak parametru email',
+				'114' => 'brak numeru telefonu',
+				'200' => 'inny chwilowy błąd',
+				'201' => 'inny chwilowy błąd bazy danych',
+				'202' => 'POS o podanym identyfikatorze jest zablokowany',
+				'203' => 'niedozwolona wartość pay_type dla danego parametru pos_id',
+				'204' => 'podana metoda płatności (wartość pay_type) jest chwilowo zablokowana dla danego parametru pos_id, np. przerwa konserwacyjna bramki płatniczej',
+				'205' => 'kwota transakcji mniejsza od wartości minimalnej',
+				'206' => 'kwota transakcji większa od wartości maksymalnej',
+				'207' => 'przekroczona wartość wszystkich transakcji dla jednego klienta w ostatnim przedziale czasowym',
+				'208' => 'POS działa w wariancie ExpressPayment lecz nie nastąpiła aktywacja tego wariantu współpracy (czekamy na zgodę działu obsługi klienta)',
+				'209' => 'błędny numer pos_id lub pos_auth_key',
+				'211' => 'nieprawidłowa waluta transakcji',
+				'212' => 'próba utworzenia transakcji częściej niż raz na minutę - dla nieaktywnej firmy',
+				'500' => 'transakcja nie istnieje',
+				'501' => 'brak autoryzacji dla danej transakcji',
+				'502' => 'transakcja rozpoczęta wcześniej',
+				'503' => 'autoryzacja do transakcji była już przeprowadzana',
+				'504' => 'transakcja anulowana wcześniej',
+				'505' => 'transakcja przekazana do odbioru wcześniej',
+				'506' => 'transakcja już odebrana',
+				'507' => 'błąd podczas zwrotu środków do Klienta',
+				'508' => 'Klient zrezygnował z płatności',
+				'599' => 'błędny stan transakcji, np. nie można uznać transakcji kilka razy lub inny, prosimy o kontakt',
+				'999' => 'inny błąd krytyczny - prosimy o kontakt',
+				'777' => 'utworzenie transakcji spowoduje przekroczenie limitu transakcji dla firmy w trakcie weryfikacji, weryfikacja odbędzie się w ciągu jednego dnia roboczego' 
+		);
 	}
 }
+
 ?>
